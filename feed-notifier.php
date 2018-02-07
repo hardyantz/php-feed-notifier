cb50988d742aec8d9e973cef9640ff7f969ff16f
Buyung Hardiansyah <hardyantz@yahoo.com>
2018-02-07 13:32:59 +0700

    test sonar

--- feed-notifier.php
+++ feed-notifier.php
@@ -28,9 +28,9 @@ function parse_feed($url) {
 
     //CONSTRUCT ARRAY
     foreach($valueall as $key => $value){
-	if($value['type'] != 'cdata') {
-	    $item[$key] = $value;
-	  }
+  	if($value['type'] != 'cdata') {
+  	    $item[$key] = $value;
+  	  }
     }
 
     $i = 0;
@@ -158,6 +158,14 @@ function deliver_mail_feed ($feed_url) {
 
     if(!mail($to, $subject, $get_feed, $headers)) echo "can't send mail";  
 
+}
+
+function delMail($res){
+  if($res){ echo "ok"} else {echo "false"}
+
+    $f = file_get_contents($res)
+
+  return $f;
 }
 
 // you can run as service or crontab for each n second
